7726cb18b70237a677fe8ca1dfccb54bb1de5397
danilacool152-sketch <danilacool152@gmail.com>
2025-12-10 01:26:25 +0300

    Update Form2.Designer.cs
    убрана бронь

--- Cinema/Form2.Designer.cs
+++ Cinema/Form2.Designer.cs
@@ -75,7 +75,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(551, 32);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Купить или забронировать билет";
+            this.button2.Text = "Купить";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
